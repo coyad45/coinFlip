373b4fb67a3a40b3703804565171feceabb276c5
Oliver <coyad45@gmail.com>
2025-06-23 21:31:44 -0300

    dando estilos al entorno

--- asset/script/script.js
+++ asset/script/script.js
@@ -1,14 +1,17 @@
 var resultado = document.getElementById("resultado");
 var imagen = document.getElementById("coin");
+var shadow = document.getElementById("shadow");
 function numeroRandom(){
     let lanzada = Math.floor(Math.random() * (1 - 0 + 1) + 0);
     if(lanzada == 1){
         resultado.innerHTML = ("Tails");
-        imagen.setAttribute("src",)
+        imagen.setAttribute("src","asset/img/tails.svg")
+        shadow.setAttribute("src","asset/img/shadow.svg")
     }
     else{
         resultado.innerHTML = ("Heads");
-        imagen.setAttribute("src",)
+        imagen.setAttribute("src","asset/img/heads.svg")
+        shadow.setAttribute("src","asset/img/shadow.svg")
     }
  
 }
